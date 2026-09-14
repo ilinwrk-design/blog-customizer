1b9123fe8c5a4038327f57557daca43ef9a38f9e
Ksandr <ilin.wrk@gmail.com>
2026-09-14 11:10:53 +0300

    git commit -m "feat: add article settings panel toggle"

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,47 @@
+import { useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const formRef = useRef<HTMLElement>(null);
+
+	const handleArrowClick = () => {
+		setIsOpen((currentState) => !currentState);
+	};
+
+	useEffect(() => {
+		if (!isOpen) {
+			return;
+		}
+
+		const handleOutsideClick = (event: MouseEvent) => {
+			const target = event.target as Node;
+
+			if (!formRef.current?.contains(target)) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('click', handleOutsideClick);
+
+		return () => {
+			document.removeEventListener('click', handleOutsideClick);
+		};
+	}, [isOpen]);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={handleArrowClick} />
+			<aside
+				ref={formRef}
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpen,
+				})}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
