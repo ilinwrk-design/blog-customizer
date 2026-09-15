13662a0fd29087d06947faabaf4c6020f7056687
Ksandr <ilin.wrk@gmail.com>
2026-09-15 10:05:52 +0300

    fix: display separator between color settings

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -114,7 +114,9 @@ export const ArticleParamsForm = ({
 						}
 					/>
 
-					<Separator />
+					<div className={styles.separator}>
+						<Separator />
+					</div>
 
 					<Select
 						title='цвет фона'
